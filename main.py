05799837de9f8c228894ec87de97ce729d7d407d
Ammaar Esmailjee <ammaar.esmailjee@gmail.com>
2025-02-18 23:49:45 -0500

    wrap the functionality into a class: Book

--- main.py
+++ main.py
@@ -3,46 +3,51 @@ import string
 from pathlib import Path
 
 
-def read_file(p: Path) -> str:
-    with p.open() as f:
-        return f.read()
+class Book:
+    def __init__(self, path: Path):
+        self.path = path
+        self.__contents = None
 
+    def read(self) -> str:
+        if not self.__contents:
+            with self.path.open() as f:
+                self.__contents = f.read()
+        return self.__contents
 
-def word_count(text: str) -> int:
-    return len(text.split())
+    def word_count(self) -> int:
+        return len(self.read().split())
 
+    def char_counts_lower(self) -> dict[str, int]:
+        counts = {}
+        for c in self.read():
+            counts[c.lower()] = counts.setdefault(c.lower(), 0) + 1
+        return counts
 
-def character_counts(text: str) -> dict[str, int]:
-    counts = {}
-    for c in text:
-        counts[c.lower()] = counts.setdefault(c.lower(), 0) + 1
-    return counts
+    def char_counts_lower_sorted_tuples(
+        self, reverse: bool = False
+    ) -> list[tuple[str, int]]:
+        return sorted(
+            [
+                (c, v)
+                for c, v in self.char_counts_lower().items()
+                if c in string.ascii_lowercase
+            ],
+            key=operator.itemgetter(1),
+            reverse=reverse,
+        )
 
-
-def letter_counts_sorted(
-    counts: dict[str, int], reverse: bool = False
-) -> list[tuple[str, int]]:
-    return sorted(
-        [(c, v) for c, v in counts.items() if c in string.ascii_lowercase],
-        key=operator.itemgetter(1),
-        reverse=reverse,
-    )
-
-
-def print_report(p: Path) -> None:
-    text = read_file(p)
-
-    print(f"--- Begin report of {p.relative_to('.')} ---")
-    print(f"{word_count(text)} words found in the document")
-    print()
-    for c, count in letter_counts_sorted(character_counts(text), reverse=True):
-        print(f"The '{c}' character was found {count} times")
-    print("--- End report ---")
+    def report(self) -> None:
+        print(f"--- Begin report of {self.path.relative_to('.')} ---")
+        print(f"{self.word_count()} words found in the document")
+        print()
+        for c, count in self.char_counts_lower_sorted_tuples():
+            print(f"The '{c}' character was found {count} times")
+        print("--- End report ---")
 
 
 def main():
-    path = Path("./books/frankenstein.txt")
-    print_report(path)
+    frankenstein = Book(Path("./books/frankenstein.txt"))
+    frankenstein.report()
 
 
 if __name__ == "__main__":
