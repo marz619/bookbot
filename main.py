cd4e4836944a2057c4ec05a48123e1d39f9ac1ae
Ammaar Esmailjee <ammaar.esmailjee@gmail.com>
2025-02-18 23:13:00 -0500

    L11: Print a report
    prints an aggregated report of the letters and their counts
    in ./books/frankenstein.txt
    
    ref: https://www.boot.dev/lessons/d6536f09-dc1d-4922-a2a5-a73c536ee09d

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
+import operator
+import string
+from pathlib import Path
+
+
+def read_file(p: Path) -> str:
+    with p.open() as f:
+        return f.read()
+
+
+def word_count(text: str) -> int:
+    return len(text.split())
+
+
 def character_counts(text: str) -> dict[str, int]:
     counts = {}
     for c in text:
-         counts[c.lower()] = counts.setdefault(c.lower(), 0) + 1
+        counts[c.lower()] = counts.setdefault(c.lower(), 0) + 1
     return counts
 
 
+def letter_counts_sorted(
+    counts: dict[str, int], reverse: bool = False
+) -> list[tuple[str, int]]:
+    return sorted(
+        [(c, v) for c, v in counts.items() if c in string.ascii_lowercase],
+        key=operator.itemgetter(1),
+        reverse=reverse,
+    )
+
+
+def print_report(p: Path) -> None:
+    text = read_file(p)
+
+    print(f"--- Begin report of {p.relative_to('.')} ---")
+    print(f"{word_count(text)} words found in the document")
+    print()
+    for c, count in letter_counts_sorted(character_counts(text), reverse=True):
+        print(f"The '{c}' character was found {count} times")
+    print("--- End report ---")
+
+
 def main():
-    text = None
-    with open("./books/frankenstein.txt") as f:
-        text = f.read()
-    char_counts = character_counts(text)
-    print(char_counts)
+    path = Path("./books/frankenstein.txt")
+    print_report(path)
 
 
 if __name__ == "__main__":
     main()
-
