a6a62ce4529aa0aca9b40ce8cfd7da2316bcd9cc
Ammaar Esmailjee <ammaar.esmailjee@gmail.com>
2025-02-24 23:05:05 -0500

    return report() output as string
    build the report in an io.StringIO buffer and
    then return it

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import operator
 import string
 import sys
+from io import StringIO
 from pathlib import Path
 
 
@@ -9,18 +10,18 @@ class Book:
         self.path = path
         self.__contents = None
 
-    def read(self) -> str:
+    def contents(self) -> str:
         if not self.__contents:
             with self.path.open() as f:
                 self.__contents = f.read()
         return self.__contents
 
     def word_count(self) -> int:
-        return len(self.read().split())
+        return len(self.contents().split())
 
     def char_counts_lower(self) -> dict[str, int]:
         counts = {}
-        for c in self.read():
+        for c in self.contents():
             counts[c.lower()] = counts.setdefault(c.lower(), 0) + 1
         return counts
 
@@ -37,24 +38,27 @@ class Book:
             reverse=reverse,
         )
 
-    def report(self) -> None:
-        print("============ BOOKBOT ============")
-        print(f"Analyzing book found at {self.path.relative_to('.')}...")
-        print("----------- Word Count ----------")
-        print(f"Found {self.word_count()} total words")
-        print("--------- Character Count -------")
+    def report(self) -> str:
+        buffer = StringIO()
+        buffer.write("============ BOOKBOT ============\n")
+        buffer.write(f"Analyzing book found at {self.path.relative_to('.')}...\n")
+        buffer.write("----------- Word Count ----------\n")
+        buffer.write(f"Found {self.word_count()} total words\n")
+        buffer.write("--------- Character Count -------\n")
         for c, count in self.char_counts_lower_sorted_tuples(reverse=True):
-            print(f"{c}: {count}")
-        print("============= END ===============")
+            buffer.write(f"{c}: {count}\n")
+        buffer.write("============= END ===============")
+        return buffer.getvalue()
 
 
 def main(args: list[str]) -> int:
     # ensure an arg has been provided
     if not len(args):
-        print("Usage: python3 main.py <path_to_book>")
+        print("Usage: python3 main.py <path_to_book> [path_to_book ...]")
         return 1
     # load the book and print the report
-    Book(Path(args[0])).report()
+    for book_path in args:
+        print(Book(Path(book_path)).report())
     return 0
 
 
